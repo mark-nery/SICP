1ecc3e71aa066c1cacdf3748277a2eeda0c0ac99
mark nery <marknery@gmail.com>
2013-10-07 17:09:39 -0600

    newtons method

--- chapter-one.scm
+++ chapter-one.scm
@@ -29,3 +29,39 @@
       0
       y))
 
+;; 1.1.7 Newtons Method
+
+(define (square x)
+  (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve-guess guess x)
+  (average (/ x guess) guess))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve-guess guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+;; 1.6
+
+(define
+  (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter* guess x)
+  "evals else resulting in overflow"
+  (new-if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve-guess guess x) x)))
+
+;; 1.7
